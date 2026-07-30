93e917191bcc6a22eab22e6b9b7858682dfb41df
David Steiner <git@davidsteiner.dev>
2026-07-30 20:58:45 +0200

    refactor: move the tokenizer into its own module (#17)

--- crates/refix-message/src/lib.rs
+++ crates/refix-message/src/lib.rs
@@ -2,5 +2,7 @@ pub mod framing;
 mod message;
 #[cfg(test)]
 mod test_utils;
+mod tokenizer;
 
-pub use self::message::{RawMessage, TokenizeError, Tokenizer};
+pub use self::message::{MALFORMED_TAG, RawMessage};
+pub use self::tokenizer::{TokenizeError, Tokenizer};
