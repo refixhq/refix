0e8291b7957fdf691f0073f4086e82e9a5496579
David Steiner <git@davidsteiner.dev>
2026-08-04 16:11:12 +0200

    Add utility function to read logs

--- python/refix-engine/python/refix/__init__.py
+++ python/refix-engine/python/refix/__init__.py
@@ -1,5 +1,6 @@
 from refix._core import MALFORMED_TAG, Garble, MessageStream, RawMessage, Tokenizer, version
+from refix.log import read_log
 
 __version__ = version()
 
-__all__ = ["MALFORMED_TAG", "Garble", "MessageStream", "RawMessage", "Tokenizer", "__version__"]
+__all__ = ["MALFORMED_TAG", "Garble", "MessageStream", "RawMessage", "read_log", "Tokenizer", "__version__"]
